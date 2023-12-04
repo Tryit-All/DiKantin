7a480ec39161fd0255e1e3bff6e3bcff7d320d12
Try IT <99704558+Tryit-All@users.noreply.github.com>
2023-12-04 21:30:26 +0700

    [Done] Dikantin Web

--- resources/views/layout/pageLogin.blade.php
+++ resources/views/layout/pageLogin.blade.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="<https://fonts.googleapis.com/icon?family=Material+Icons>" rel="stylesheet" />
+    <link rel="stylesheet" href="{{ asset('build/assets/app-0d576ebc.css') }}">
     {{-- @vite('resources/css/app.css') --}}
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     <script src="<https://cdn.tailwindcss.com>"></script>
     <link rel="shortcut icon" href="{{ URL::asset('img/logo-kotak.png') }}">
     <title>Dikantin | Login</title>
