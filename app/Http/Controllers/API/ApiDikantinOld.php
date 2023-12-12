394608d0e3c97af101b612a20b22bc940d801ffd
Try IT <99704558+Tryit-All@users.noreply.github.com>
2023-12-13 05:22:48 +0700

    [Update] RHP

--- app/Http/Controllers/API/ApiDikantinOld.php
+++ app/Http/Controllers/API/ApiDikantinOld.php
@@ -540,7 +540,7 @@ class ApiDikantinOld extends Controller
                     'menu.nama',
                     'menu.harga',
                     DB::raw('SUM(detail_transaksi.QTY) as total_qty'),
-                    DB::raw('SUM(detail_transaksi.subtotal_bayar) as total_pendapatan')
+                    (int) DB::raw('SUM(detail_transaksi.subtotal_bayar) as total_pendapatan')
                 )
                 ->where('kantin.id_kantin', $idkatin)
                 ->where('transaksi.status_pengiriman', 'terima')
